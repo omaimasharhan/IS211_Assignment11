0ca1c533180806ed4c37713d45f204c74993f1c8
Omaima <omaimasharhan@gmail.com>
2023-11-21 17:02:53 -0500

    update

--- todoapp.py
+++ todoapp.py
@@ -5,27 +5,29 @@ to_do_list = []
 
 @app.route('/', methods=['GET', 'POST'])
 def view_to_do_list():
-    error = None  # Define the error variable initially
+    error = None
 
     if request.method == 'POST':
-        if request.form['action'] == 'clear':
-            to_do_list.clear()
-        else:
-            task = request.form['task']
-            email = request.form['email']
-            priority = request.form['priority']
-
-            if not task.strip():
-                error = 'Task is empty.'
-            elif not email.strip() or '@' not in email:
-                error = 'Invalid email format.'
-            elif priority not in ('Low', 'Medium', 'High'):
-                error = 'Invalid priority choice.'
-            else:
-                to_do_list.append((task, email, priority))
+        if 'action' in request.form:
+            if request.form['action'] == 'clear':
+                to_do_list.clear()
                 return redirect('/')
+
+        task = request.form.get('task')
+        email = request.form.get('email')
+        priority = request.form.get('priority')
+
+        if not task.strip():
+            error = 'Task is empty.'
+        elif not email.strip() or '@' not in email:
+            error = 'Invalid email format.'
+        elif priority not in ('Low', 'Medium', 'High'):
+            error = 'Invalid priority choice.'
+        else:
+            to_do_list.append((task, email, priority))
+            return redirect('/')
 
     return render_template('todo_list.html', to_do_list=to_do_list, error=error)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
