3b7bdb47088c6a0fc2908f570ee8e795389ea350
Omaima <omaimasharhan@gmail.com>
2023-11-23 16:57:58 -0500

    update

--- todoapp.py
+++ todoapp.py
@@ -3,15 +3,32 @@ from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 to_do_list = []
 
+def write_tasks_to_file(tasks):
+    with open('tasks.txt', 'w') as file:
+        for task in tasks:
+            file.write(f"{task[0]}, {task[1]}, {task[2]}\n")
+
+# Function to read tasks from file
+def read_tasks_from_file():
+    to_do_list.clear()
+    with open('tasks.txt', 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            task_data = line.strip().split(', ')
+            to_do_list.append((task_data[0], task_data[1], task_data[2]))
+
+# Read tasks from file when the app starts
+read_tasks_from_file()
+
 @app.route('/', methods=['GET', 'POST'])
 def view_to_do_list():
     error = None
 
     if request.method == 'POST':
-        if 'action' in request.form:
-            if request.form['action'] == 'clear':
-                to_do_list.clear()
-                return redirect('/')
+        if 'clear_button' in request.form:
+            to_do_list.clear()
+            write_tasks_to_file(to_do_list)
+            return redirect('/')
 
         task = request.form.get('task')
         email = request.form.get('email')
@@ -25,9 +42,16 @@ def view_to_do_list():
             error = 'Invalid priority choice.'
         else:
             to_do_list.append((task, email, priority))
+            write_tasks_to_file(to_do_list)
             return redirect('/')
 
     return render_template('todo_list.html', to_do_list=to_do_list, error=error)
+
+@app.route('/clear', methods=['POST'])
+def clear_to_do_list():
+    to_do_list.clear()
+    write_tasks_to_file(to_do_list)
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
