69841e23d577ae80f55a3a31e45bc136e382c9f5
Omaima <omaimasharhan@gmail.com>
2023-11-23 18:52:53 -0500

    update

--- todoapp.py
+++ todoapp.py
@@ -1,57 +1,36 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-to_do_list = []
 
-def write_tasks_to_file(tasks):
-    with open('tasks.txt', 'w') as file:
-        for task in tasks:
-            file.write(f"{task[0]}, {task[1]}, {task[2]}\n")
+todo_list = [
+    {'task': 'Buy groceries', 'email': 'example@email.com', 'priority': 'High'},
+    {'task': 'Finish assignment', 'email': 'another@example.com', 'priority': 'Medium'},
 
-# Function to read tasks from file
-def read_tasks_from_file():
-    to_do_list.clear()
-    with open('tasks.txt', 'r') as file:
-        lines = file.readlines()
-        for line in lines:
-            task_data = line.strip().split(', ')
-            to_do_list.append((task_data[0], task_data[1], task_data[2]))
+]
 
-# Read tasks from file when the app starts
-read_tasks_from_file()
+@app.route('/')
+def view_todo_list():
+    return render_template('todo_list.html', todo_list=todo_list)
 
-@app.route('/', methods=['GET', 'POST'])
-def view_to_do_list():
-    error = None
+@app.route('/submit', methods=['POST'])
+def submit_todo_item():
+    task = request.form['task']
+    email = request.form['email']
+    priority = request.form['priority']
 
-    if request.method == 'POST':
-        if 'clear_button' in request.form:
-            to_do_list.clear()
-            write_tasks_to_file(to_do_list)
-            return redirect('/')
-
-        task = request.form.get('task')
-        email = request.form.get('email')
-        priority = request.form.get('priority')
-
-        if not task.strip():
-            error = 'Task is empty.'
-        elif not email.strip() or '@' not in email:
-            error = 'Invalid email format.'
-        elif priority not in ('Low', 'Medium', 'High'):
-            error = 'Invalid priority choice.'
-        else:
-            to_do_list.append((task, email, priority))
-            write_tasks_to_file(to_do_list)
-            return redirect('/')
-
-    return render_template('todo_list.html', to_do_list=to_do_list, error=error)
+    todo_list.append({'task': task, 'email': email, 'priority': priority})
+    return redirect(url_for('view_todo_list'))
 
 @app.route('/clear', methods=['POST'])
-def clear_to_do_list():
-    to_do_list.clear()
-    write_tasks_to_file(to_do_list)
-    return redirect('/')
+def clear_todo_list():
+    todo_list.clear()
+    return redirect(url_for('view_todo_list'))
+
+@app.route('/delete/<int:item_index>', methods=['POST'])
+def delete_todo_item(item_index):
+    if 0 <= item_index < len(todo_list):
+        del todo_list[item_index]
+    return redirect(url_for('view_todo_list'))
 
 if __name__ == '__main__':
     app.run(debug=True)
